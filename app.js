b7e958733995ecaef477f3a566ed81ce3517642e
Bramastana Dewangga <brm.stnd@gmail.com>
2019-10-31 22:51:34 +0700

    [brm-stnd] fix bug favicon

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('trust proxy', 1);
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
